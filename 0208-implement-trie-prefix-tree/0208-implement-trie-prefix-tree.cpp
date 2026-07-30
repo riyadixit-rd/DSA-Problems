e73b174ae6edb0519d4f3c96f17ee84868f8e51f
RIYA DIXIT <dixitriyaspcc@gmail.com>
2026-07-30 12:36:11 +0530

    Time: 25 ms (34.24%), Space: 50.6 MB (54.52%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,43 @@
+class Trie {
+    struct Node {
+        Node* child[26] = {};
+        bool end = false;
+    };
+
+    Node* root;
+
+public:
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(string word) {
+        Node* cur = root;
+        for (char c : word) {
+            int i = c - 'a';
+            if (!cur->child[i]) cur->child[i] = new Node();
+            cur = cur->child[i];
+        }
+        cur->end = true;
+    }
+
+    bool search(string word) {
+        Node* cur = root;
+        for (char c : word) {
+            int i = c - 'a';
+            if (!cur->child[i]) return false;
+            cur = cur->child[i];
+        }
+        return cur->end;
+    }
+
+    bool startsWith(string prefix) {
+        Node* cur = root;
+        for (char c : prefix) {
+            int i = c - 'a';
+            if (!cur->child[i]) return false;
+            cur = cur->child[i];
+        }
+        return true;
+    }
+};
