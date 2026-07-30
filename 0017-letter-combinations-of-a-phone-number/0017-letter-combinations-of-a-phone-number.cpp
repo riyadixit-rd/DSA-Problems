6fb83e3b9135df01fe666f9127579d61e8057b13
RIYA DIXIT <dixitriyaspcc@gmail.com>
2026-07-30 12:32:20 +0530

    Time: 0 ms (100%), Space: 9.4 MB (45.71%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-
     vector<string> ans;
 
     vector<string> mp = {
@@ -11,27 +10,24 @@ public:
         "tuv", "wxyz"
     };
 
-    void solve(string digits, int idx, string curr) {
-
-        if(idx == digits.size()) {
+    void solve(string &digits, int idx, string &curr) {
+        if (idx == digits.size()) {
             ans.push_back(curr);
             return;
         }
 
-        string letters = mp[digits[idx] - '0'];
-
-        for(char ch : letters) {
-            solve(digits, idx + 1, curr + ch);
+        for (char ch : mp[digits[idx] - '0']) {
+            curr.push_back(ch);
+            solve(digits, idx + 1, curr);
+            curr.pop_back();
         }
     }
 
     vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
 
-        if(digits.empty())
-            return {};
-
-        solve(digits, 0, "");
-
+        string curr;
+        solve(digits, 0, curr);
         return ans;
     }
 };
