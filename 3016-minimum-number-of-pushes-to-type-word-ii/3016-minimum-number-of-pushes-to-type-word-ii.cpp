9d49eafaccabb026a59ea64e65b039605b9ac0c6
RIYA DIXIT <dixitriyaspcc@gmail.com>
2026-07-31 13:41:19 +0530

    Time: 16 ms (33.38%), Space: 28.5 MB (25.39%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> freq(26, 0);
+
+        for (char c : word)
+            freq[c - 'a']++;
+
+        sort(freq.rbegin(), freq.rend());
+
+        int ans = 0;
+        for (int i = 0; i < 26 && freq[i]; i++)
+            ans += freq[i] * (i / 8 + 1);
+
+        return ans;
+    }
+};
