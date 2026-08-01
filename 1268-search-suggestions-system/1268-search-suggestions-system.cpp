ea7c2eb99e0a284af8f550f846344d9d37faa8b0
RIYA DIXIT <dixitriyaspcc@gmail.com>
2026-08-01 15:21:55 +0530

    Time: 14 ms (79.89%), Space: 32.1 MB (85.41%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+
+        sort(products.begin(), products.end());
+
+        vector<vector<string>> ans;
+        string prefix = "";
+
+        for (char c : searchWord) {
+            prefix += c;
+
+            auto it = lower_bound(products.begin(), products.end(), prefix);
+
+            vector<string> temp;
+
+            for (int i = 0; i < 3 && it + i != products.end(); i++) {
+                string &word = *(it + i);
+
+                if (word.compare(0, prefix.size(), prefix) == 0)
+                    temp.push_back(word);
+                else
+                    break;
+            }
+
+            ans.push_back(temp);
+        }
+
+        return ans;
+    }
+};
