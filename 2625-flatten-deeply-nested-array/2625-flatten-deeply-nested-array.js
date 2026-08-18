e173c250de3a3c410e8b127baa064a1a5334d077
RIYA DIXIT <dixitriyaspcc@gmail.com>
2026-08-18 13:48:38 +0530

    Time: 163 ms (15.95%), Space: 93.7 MB (8.56%) - LeetHub

---
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -0,0 +1,14 @@
+var flat = function(arr, n) {
+    if (n === 0) return arr;
+
+    let ans = [];
+
+    for (let x of arr) {
+        if (Array.isArray(x))
+            ans.push(...flat(x, n - 1));
+        else
+            ans.push(x);
+    }
+
+    return ans;
+};
