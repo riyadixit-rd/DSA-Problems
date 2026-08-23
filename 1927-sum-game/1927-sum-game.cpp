a91408e1be66bbe8ee2c946a02d97889692d46a6
RIYA DIXIT <dixitriyaspcc@gmail.com>
2026-08-23 13:58:23 +0530

    Time: 0 ms (100%), Space: 14.1 MB (33.57%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+        int diff = 0, q1 = 0, q2 = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            if (num[i] == '?') q1++;
+            else diff += num[i] - '0';
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            if (num[i] == '?') q2++;
+            else diff -= num[i] - '0';
+        }
+
+        if (q1 == q2)
+            return diff != 0;
+
+        if ((q1 - q2) % 2 != 0)
+            return true;
+
+        return diff != (q2 - q1) / 2 * 9;
+    }
+};
